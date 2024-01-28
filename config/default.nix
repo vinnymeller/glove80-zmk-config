2723f627f3de3e09338f6eba7ff20b2e89e0e220
Vinny Meller <vinnymeller@proton.me>
2024-01-27 19:24:27 -0500

    test flake

--- config/default.nix
+++ config/default.nix
@@ -1,10 +1,17 @@
-{ pkgs ?  import <nixpkgs> {} }:
-
-let
-  firmware = import ../src {};
+{pkgs ? import <nixpkgs> {}}: let
+  # firmware = import ../src {};
+  firmware = import ../zmk {};
   config = ./.;
 
-  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
-  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
-
-in firmware.combine_uf2 glove80_left glove80_right
+  glove80_left = firmware.zmk.override {
+    board = "glove80_lh";
+    keymap = "${config}/glove80.keymap";
+    kconfig = "${config}/glove80.conf";
+  };
+  glove80_right = firmware.zmk.override {
+    board = "glove80_rh";
+    keymap = "${config}/glove80.keymap";
+    kconfig = "${config}/glove80.conf";
+  };
+in
+  firmware.combine_uf2 glove80_left glove80_right
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Glove80 ZMK Configuration Builder";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    # nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      # pkgs = import nixpkgs {inherit system;};
+      config = ./.;
+      firmware = import ./zmk {};
+
+      glove80_left = firmware.zmk.override {
+        board = "glove80_lh";
+        keymap = "${config}/glove80.keymap";
+        kconfig = "${config}/glove80.conf";
+      };
+      glove80_right = firmware.zmk.override {
+        board = "glove80_rh";
+        keymap = "${config}/glove80.keymap";
+        kconfig = "${config}/glove80.conf";
+      };
+    in {
+      defaultPackage = firmware.combine_uf2 glove80_left glove80_right;
+    });
+}
